e0ea4afb20b519940cd9c6ef68c1c367c21f1f07
Joe Ow <joeow@Joes-MacBook.local>
2024-06-19 20:39:22 +0800

    update

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -11,8 +11,6 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-void	ft_putchar_fd(char c, int fd);
-
 void	ft_putnbr_fd(int n, int fd)
 {
 	long	nb;
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -14,8 +14,6 @@
 #include <unistd.h>
 */
 #include "libft.h"
-
-size_t	ft_strlen(const char *s);
 /*
 #include <string.h>
 #include <ctype.h>
